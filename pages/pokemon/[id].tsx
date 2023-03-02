a428769eb12332ff76488a92917e1bd944ece950
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2023-03-01 23:45:16 -0300

    Pokemon App | Se aplica Incremental Side Rendering - ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -103,17 +103,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     //     },
     //   },
     // ],
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon: pokemon,
     },
+    revalidate: 86400,
   };
 };
 
